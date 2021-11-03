3627178c8a252c335d0f2af0dc515ef3472ad1d4
2MariusG <73899587+2MariusG@users.noreply.github.com>
2021-11-03 19:52:25 +0200

    search todos

--- todos/app.js
+++ todos/app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
-const list = document.querySelector('.todos')
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const generateTemplate = todo => {
     
@@ -17,11 +18,35 @@ addForm.addEventListener('submit', e => {
     e.preventDefault();
     const todo = addForm.add.value.trim();
 
-    if (todo.length){
+    if (todo.length) {
         generateTemplate(todo);
         addForm.reset();
-    } 
-    
-    
+    }
+});
 
-})
+//delete todos
+
+list.addEventListener('click', e => {
+    
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
+
+const filterTodos = (term) => {
+
+    Array.from(list.children)
+        .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add('filtered'));
+    
+    Array.from(list.children)
+        .filter((todo) => todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.remove('filtered'));
+};
+//keyup event
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
+});
+
